e8d4fd410789a6b4b9d5d4fd3f2c80d8dd7fe055
tonky0110 <tonky0110@gmail.com>
2020-04-07 22:27:55 +0900

    2.0 init ToDo

--- src/index.js
+++ src/index.js
@@ -1,36 +1,19 @@
 import { createStore } from 'redux';
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
-number.innerText = 0;
-const ADD = 'ADD';
-const MINUS = 'MINUS';
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-const countModifier = (count = 0, action) => {
-  console.log(count, action);
-  switch(action.type){
-    case ADD:
-      return count + 1;
-    case MINUS:
-      return count - 1;
-    default:
-      return count;
-  }
+const createToDo = (toDo) => {
+  const li = document.createElement('li');
+  li.innerText = toDo;
+  ul.appendChild(li);
 };
 
-const countStore = createStore(countModifier);
-const onChange = () => {
-  number.innerText = countStore.getState();
+const onsubmit = (e) => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = '';
+  createToDo(toDo);
 }
-const handleAdd = () => {
-  countStore.dispatch({type: ADD});
-}
-const handleMinus = () => {
-  countStore.dispatch({type: MINUS});
-}
-countStore.subscribe(onChange);
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
-// add.addEventListener("click", () => countStore.dispatch({type: "ADD"}));
-// minus.addEventListener("click", () => countStore.dispatch({type: "MINUS"}));
+form.addEventListener("submit", onsubmit);
