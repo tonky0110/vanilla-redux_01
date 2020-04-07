5493b0495eb5fe50d343b7bd9b3f9d6808f37531
tonky0110 <tonky0110@gmail.com>
2020-04-07 22:47:54 +0900

    2.0 set store and reducer

--- src/index.js
+++ src/index.js
@@ -4,16 +4,32 @@ const form = document.querySelector("form");
 const input = document.querySelector("input");
 const ul = document.querySelector("ul");
 
-const createToDo = (toDo) => {
-  const li = document.createElement('li');
-  li.innerText = toDo;
-  ul.appendChild(li);
+const ADD_TODO = "ADD_TODO"; 
+const DELETE_TODO = "DELETE_TODO";
+
+const reducer = (state = [], action) => {
+  console.log('action: ', action);
+  switch(action.type){
+    case ADD_TODO:
+      return [];
+    case DELETE_TODO:
+      return [];
+    default:
+      return state;
+  }
 };
+
+const store = createStore(reducer);
+
+store.subscribe( )
 
 const onsubmit = (e) => {
   e.preventDefault();
   const toDo = input.value;
   input.value = '';
-  createToDo(toDo);
+  store.dispatch({
+    type: ADD_TODO,
+    text: input.value
+  });
 }
 form.addEventListener("submit", onsubmit);
