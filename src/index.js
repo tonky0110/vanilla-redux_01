60b76da31bbfd2d9f40cda7383cfc13475cfba88
tonky0110 <tonky0110@gmail.com>
2020-04-07 21:11:31 +0900

    1.1 redux createStore and Reducer

--- src/index.js
+++ src/index.js
@@ -1,25 +1,14 @@
+import { createStore } from 'redux';
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
+const countModifier = (state = 0) => {
+  return state;
+};
 
-number.innerText = count;
+const countStore = createStore(countModifier);
 
 
-const updateText = () => {
-  number.innerText = count;
-}
-const handleAdd = () => {
-  console.log("Add");
-  count++;
-  updateText();
-}
-const handleMinus = () => {
-  console.log("Minus");
-  count--;
-  updateText();
-}
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus); 
+console.log(countStore.getState());
