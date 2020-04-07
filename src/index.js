71b304926b875c78cd80db0f9ea45e390c25fae0
tonky0110 <tonky0110@gmail.com>
2020-04-07 22:08:25 +0900

    1.3 redux subscribe(store가 변경되면, 자동으로 호출됨)

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
 const countModifier = (state = 0, action) => {
+  console.log(state, action);
   if (action.type === "ADD"){
     return state + 1;
   } else if (action.type === 'MINUS') {
@@ -15,12 +16,17 @@ const countModifier = (state = 0, action) => {
 };
 
 const countStore = createStore(countModifier);
-countStore.dispatch({type: "ADD"});
-countStore.dispatch({type: "ADD"});
-countStore.dispatch({type: "ADD"});
-countStore.dispatch({type: "ADD"});
-countStore.dispatch({type: "ADD"});
-countStore.dispatch({type: "ADD"});
-countStore.dispatch({type: "MINUS"});
-
-console.log(countStore.getState());
+const onChange = () => {
+  number.innerText = countStore.getState();
+}
+const handleAdd = () => {
+  countStore.dispatch({type: "ADD"});
+}
+const handleMinus = () => {
+  countStore.dispatch({type: "MINUS"});
+}
+countStore.subscribe(onChange);
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
+// add.addEventListener("click", () => countStore.dispatch({type: "ADD"}));
+// minus.addEventListener("click", () => countStore.dispatch({type: "MINUS"}));
