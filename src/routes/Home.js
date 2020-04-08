e9924bb44800c6925b3a57cbb0e8323e3143f44c
tonky0110 <tonky0110@gmail.com>
2020-04-08 22:38:04 +0900

    dispatch addToDo

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import {addToDo, deleteToDo} from '../store';
 
-function Home() {
+function Home(props) {
     const [ text, setText ] = useState("");
     function onChange(e) {
         setText(e.target.value);
@@ -8,8 +10,9 @@ function Home() {
     function onSubmit(e) {
         e.preventDefault();
         setText("");
+        props.add(text);
     }
-
+    console.log("props:", props);
     return (
         <>
             <h1>To Do</h1>
@@ -18,9 +21,21 @@ function Home() {
                 <button>Add</button>
             </form>
             <ul>
+                {
+                    JSON.stringify(props.toDos)
+                }
             </ul>
         </>
     );
 };
 
-export default Home;
+const mapStateToProps = state => {
+    return ({toDos: state});
+}
+const mapDispatchToProps = dispatch => {
+    return ({
+        add: (text) => dispatch(addToDo(text)),
+        delete: (id) => dispatch(deleteToDo(id))
+    });
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
--- src/store.js
+++ src/store.js
@@ -18,7 +18,7 @@ export const deleteToDo = (id) => {
 const reducer = (state = [], action) => {
     switch (action.type) {
         case ADD:
-            return [{text: action.text, id: new Date.now()}, ...state];
+            return [{text: action.text, id: Date.now()}, ...state];
         case DELETE:
             return state.filter(toDo => toDo.id !== action.id);
         default:
